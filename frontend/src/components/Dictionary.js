eadf5b915d3b363987f68424073709ea0fd7dd6e
htkchi2212-collab <htkchi2212@gmail.com>
2025-12-18 12:39:00 +0700

    chore: remove all files from repository

--- frontend/src/components/Dictionary.js
+++
@@ -1,8 +0,0 @@
-export const dictionaryData = {
-  coffee: {
-    word: "coffee",
-    definition: "A hot drink made from roasted coffee beans.",
-    example: "I drink coffee every morning.",
-  },
-};
-
